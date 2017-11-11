dce90e4e6cddf9c52f3563c356e0fa842b619410
amair <amair@LAPTOP-Q9P3M1OQ>
2017-11-10 23:05:39 -0600

    Add project files.

---
+++ SumaMatriz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaMatriz
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                int[,] m = { { 8, 6, 4 }, { 9, 3, 2 }, { 5, 1, 7 } };
+                int Suma = 0;
+                for (int x = 0; x < 3; x++)
+                {
+                    Suma = Suma + m[x, x];
+
+                }
+                Console.WriteLine(" La suma de la diagonal es:" + Suma);
+                Console.ReadKey();
+            }
+        }
+    }
